d1e9e8ea6913ecfce1b9be648db3e55fb3c3330b
mclima <mcamil2009@gmail.com>
2024-04-18 21:04:05 -0400

    initial commit

---
+++ api/recipe.controllers.js
@@ -0,0 +1,100 @@
+const Recipe = require("./recipe.model");
+
+exports.findAll = function (req, res) {
+  Recipe.find({}).then((data) => res.send(data));
+};
+
+exports.findById = (req, res) => {
+  const id = req.params.id;
+  Recipe.findOne({ _id: id }).then((data) => res.send(data));
+};
+
+exports.add = function (req, res) {
+  Recipe.create(req.body).then(res.sendStatus(202));
+};
+
+exports.update = function (req, res) {
+  console.log(req.body);
+  const id = req.params.id;
+  Recipe.findByIdAndUpdate(id, req.body, { new: true }).then((data) =>
+    res.send(data)
+  );
+};
+
+exports.delete = function (req, res) {
+  let id = req.params.id;
+  Recipe.deleteOne({ _id: id }).then(res.sendStatus(202));
+};
+
+exports.upload = function (req, res) {
+  console.log(req.files);
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  let file = req.files.file;
+  file.mv(`./static/img/${req.body.filename}`, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({ file: `static/img/${req.body.filename}` });
+    console.log(res.json);
+  });
+  return res.sendStatus(200);
+};
+
+exports.killall = function (req, res) {
+  Recipe.deleteMany({}).then(res.sendStatus(202));
+};
+
+exports.import = function (req, res) {
+  Recipe.create([
+    {
+      title: "Lasagna",
+      description:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+      image: "lasagna.png",
+      ingredients: ["salt", "honey", "sugar", "rice", "walnuts", "lime juice"],
+      preparation: [
+        { step: "Boil water" },
+        { step: "Fry the eggs" },
+        { step: "Serve hot" },
+      ],
+    },
+    {
+      title: "Pho-Chicken Noodle Soup",
+      description:
+        'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
+      image: "pho.png",
+      ingredients: ["salt", "honey", "sugar", "rice", "walnuts", "lime juice"],
+      preparation: [
+        { step: "Boil water" },
+        { step: "Fry the eggs" },
+        { step: "Serve hot" },
+      ],
+    },
+    {
+      title: "Guacamole",
+      description:
+        "Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.",
+      image: "guacamole.png",
+      ingredients: ["salt", "honey", "sugar", "rice", "walnuts", "lime juice"],
+      preparation: [
+        { step: "Boil water" },
+        { step: "Fry the eggs" },
+        { step: "Serve hot" },
+      ],
+    },
+    {
+      title: "Hamburger",
+      description:
+        "A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.",
+      image: "hamburger.png",
+      ingredients: ["salt", "honey", "sugar", "rice", "walnuts", "lime juice"],
+      preparation: [
+        { step: "Boil water" },
+        { step: "Fry the eggs" },
+        { step: "Serve hot" },
+      ],
+    },
+  ]).then(res.sendStatus(202));
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+require("dotenv").config();
+const recipeControllers = require("./api/recipe.controllers");
+
+const app = express();
+const dataBaseURL = process.env.DATABASE;
+
+app.use(express.static("static"));
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/static/index.html");
+});
+
+app.get("/api/recipes", recipeControllers.findAll);
+app.get("/api/recipes/:id", recipeControllers.findById);
+app.post("/api/recipes", recipeControllers.add);
+app.put("/api/recipes/:id", recipeControllers.update);
+app.delete("/api/recipes/:id", recipeControllers.delete);
+app.get("/api/import", recipeControllers.import);
+app.get("/api/killall", recipeControllers.killall);
+app.post("/api/upload", recipeControllers.upload);
+
+mongoose
+  .connect(dataBaseURL, {})
+  .then(() => console.log("MongoDb connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
